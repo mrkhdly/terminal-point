827b466877473d4a72e17dfe0ccc25c5a5948ed7
Mark Hadley <markhdly@gmail.com>
2017-01-05 20:13:29 -0500

    Changed gulpfile.js & added folder

--- gulpfile.js
+++ gulpfile.js
@@ -6,15 +6,23 @@ var concat = require('gulp-concat');
 var cssnano = require('gulp-cssnano');
 var uncss = require('gulp-uncss');
 
-
 gulp.task('css', function() {
-    return gulp.src(['./node_modules/tachyons*/css/*.min.css'])
-      .pipe(concat('main.css'))
-      .pipe(cssnano())
-      .pipe(gulp.dest('./css'));
+  return gulp.src(['./node_modules/tachyons*/css/*.min.css'])
+        .pipe(concat('main.css'))
+        .pipe(gulp.dest('./css'))
 });
 
-gulp.task('jekyll', function () {
+gulp.task('clean', function () {
+    return gulp.src(['./node_modules/tachyons*/css/*.min.css'])
+        .pipe(concat('main.css'))
+        .pipe(uncss({
+            html: ['index.html', './_site/**/*.*']
+        }))
+        .pipe(cssnano())
+        .pipe(gulp.dest('./_site/css'));
+});
+
+gulp.task('build', function () {
   return gulp.src('*.js', {read: false})
     .pipe(shell([
       'jekyll build --watch'
@@ -32,4 +40,4 @@ gulp.task('serve', function () {
     gulp.watch(['./_*/**/*.*', './images/**/*.*', './index.html', '!./_site/**/*.*']).on('change', reload);
 });
 
-gulp.task('default', ['jekyll', 'css', 'serve']);
+gulp.task('default', ['css', 'build', 'serve']);
